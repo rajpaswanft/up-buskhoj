e62b4f05744a00d0e5ee6340cfb7e30b3673cb80
Raj Paswan <rajpaswanft@gmail.com>
2026-09-04 00:14:24 +0530

    Create stops_coords.js

---
+++ districts/js/stops_coords.js
@@ -0,0 +1,35 @@
+// Master Coordinates Database (English Keys)
+const STOPS_COORDINATES = {
+  // Deoria City Hub
+  "Deoria Sadar Bus Station": [26.5020, 83.7791],
+  "Kacheri Bus Stand": [26.5058, 83.7842],
+  "Kasya Dhala": [26.5112, 83.7885],
+  "Overbridge / SSBL Gate": [26.5005, 83.7750],
+  "Purva Chauraha": [26.4950, 83.7820],
+
+  // Tehsil & Rural Stops
+  "Salempur Bus Stand": [26.3015, 83.9248],
+  "Rudrapur Bus Station": [26.4385, 83.6214],
+  "Barhaj Bus Stand": [26.2842, 83.7540],
+  "Bhatpar Rani": [26.2482, 84.0531],
+  "Lar Road / Lar": [26.2085, 83.9712],
+  "Bhatni Bus Stand": [26.5684, 83.9721],
+  "Gauri Bazar": [26.5861, 83.6933],
+  "Gauri Bazar Link": [26.5861, 83.6933],
+  "Baitalpur": [26.5505, 83.7431],
+  "Tarkulwa": [26.6185, 83.8760],
+  "Rampur Karkhana": [26.5695, 83.8210],
+  "Khukhundoo": [26.3980, 83.8540],
+  "Bhagalpur": [26.2150, 83.8230],
+  "Kaparwar Ghat": [26.2750, 83.6890],
+  "Mail": [26.3380, 83.7310],
+  "Mehrauna Ghat (Bihar Border)": [26.1750, 84.0120],
+
+  // Connecting Highway Corridors
+  "Chauri Chaura": [26.6432, 83.6062],
+  "Gorakhpur Bus Stand": [26.7588, 83.3813],
+  "Dohrighat": [26.2612, 83.5234],
+  "Mau": [25.9416, 83.5602],
+  "Saidpur": [25.5492, 83.1932],
+  "Varanasi Cantt ISBT": [25.3284, 82.9868]
+};
